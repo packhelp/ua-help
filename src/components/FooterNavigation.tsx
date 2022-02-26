3cc695e4c5cd6ad7fef143ae5b40b7be1a92930a
Bartosz Stoiński <bartosz@Bartoszs-MBP-2.ht.home>
2022-02-26 22:04:01 +0100

    simple footer

--- src/components/FooterNavigation.tsx
+++ src/components/FooterNavigation.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { chakra, Flex, useColorModeValue } from "@chakra-ui/react"
 
 export const FooterNavigation = () => {
+  const bg = useColorModeValue("white", "blue.500")
   return (
-    <div>footer</div>
+    <chakra.footer
+      bg={bg}
+      w="full"
+      px={{ base: 2, sm: 4 }}
+      py={4}
+      shadow="md"
+    >
+      <Flex justifyContent={"center"}>
+        {"Poland <3 Ukraine"}
+      </Flex>
+    </chakra.footer>
   )
 }
