bb88804019aa56aa550073a4ba7d120139537cf5
Bartosz Stoiński <bartosz@Bartoszs-MBP-2.ht.home>
2022-02-26 21:00:23 +0100

    fix

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "next/link"
 import { Button } from "@chakra-ui/react"
-import { getRouteNameForLocale, Locales, Routes } from "../utils/routes"
+import { getRouteNameForLocale, Locale, Routes } from "../utils/routes"
 import { useRouter } from "next/router";
 
 type NavigationLinkProps = {
@@ -12,7 +12,7 @@ export const NavigationLink = (props: NavigationLinkProps) => {
   const { route } = props
   const { locale } = useRouter();
 
-  const currentLocale = locale as Locales
+  const currentLocale = locale as Locale
 
   return (
     <Link href={route}>
