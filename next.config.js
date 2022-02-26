fb82dd8db61a3cc35e161b10482e4bd8619854b4
Bartosz Stoiński <bartosz@Bartoszs-MBP-2.ht.home>
2022-02-26 19:40:18 +0100

    layout init + i18n config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en-US", "pl-PL", "uk-UA"],
+    defaultLocale: "en-US",
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,14 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <div>header</div>
+      <Component {...pageProps} />
+      <div>footer</div>
+    </div>
+  )
+  // return <Component {...pageProps} />
 }
 
 export default MyApp
