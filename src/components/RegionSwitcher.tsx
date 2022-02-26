44989515c8ee87cb4403a5b06028c6801098b4b1
Bartosz Stoiński <bartosz@Bartoszs-MBP-2.ht.home>
2022-02-26 22:15:36 +0100

    fix

--- src/components/RegionSwitcher.tsx
+++ src/components/RegionSwitcher.tsx
@@ -1,17 +1,19 @@
 import React from "react"
 import { useRouter } from 'next/router';
-import { LocaleFlags, LocaleNames, locales } from "../utils/routes"
+import { Locale, LocaleFlags, LocaleNames, locales } from "../utils/routes"
 import Link from "next/link"
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu"
 
 export const RegionSwitcher = () => {
   const { asPath, locale } = useRouter();
 
+  const currentLocale = locale as Locale
+
   return (
     <div>
       <Menu>
         <MenuButton>
-          {LocaleFlags[locale]}
+          {LocaleFlags[currentLocale]}
         </MenuButton>
 
         <MenuList>
